c2820657696474dd60ea96fbb57121dfe490b809
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-08-31 05:53:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   Dio: 'Holy Diver'
 }
 
-function updatePlaylist(playlist, artistName, songTitle) = {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
